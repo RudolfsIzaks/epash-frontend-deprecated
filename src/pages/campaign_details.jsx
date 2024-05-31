634eabf8b922626b300ec1f1261b62c0dd2bff7c
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-05-31 18:58:51 +0300

    Changing status coloring

--- src/pages/campaign_details.jsx
+++ src/pages/campaign_details.jsx
@@ -5,20 +5,45 @@ function CampaignDetail() {
   const location = useLocation();
   const { campaignGroup } = location.state;
 
+  const getStatusStyle = (status) => {
+    switch (status.toLowerCase()) {
+      case "incomplete":
+        return { color: "red" };
+      case "ready to deploy":
+        return { color: "yellow" };
+      case "complete":
+        return { color: "green" };
+      default:
+        return { color: "black" };
+    }
+  };
+
   return (
     <div className="flex justify-center py-20">
       <div className="flex flex-col justify-evenly gap-5 p-5">
-        <h2 className="font-custom font-black text-epash-green text-4xl">{campaignGroup[0].name}</h2>
-        <p className="text-black font-custom font-semibold text-2xl">{campaignGroup[0].status}</p>
+        <h2 className="font-custom font-black text-epash-green text-4xl">
+          {campaignGroup[0].name}
+        </h2>
+        <p
+          style={getStatusStyle(campaignGroup[0].status)}
+          className="text-black font-custom font-semibold text-2xl"
+        >
+          {campaignGroup[0].status}
+        </p>
         <div className="grid grid-cols-2 gap-10">
           {campaignGroup.map((campaign, index) => (
-            <div key={index} className="ad-variation flex flex-col justify-center items-center w-96 bg-white p-5 shadow-md rounded-lg">
+            <div
+              key={index}
+              className="ad-variation flex flex-col justify-center items-center w-96 bg-white p-5 shadow-md rounded-lg"
+            >
               <img
                 className="rounded-md w-80 mb-5"
                 src={campaign.image_link}
                 alt={`Ad variation ${index + 1}`}
               />
-              <p className="font-semibold italic font-custom p-5">{campaign.ad_text}</p>
+              <p className="font-semibold italic font-custom p-5">
+                {campaign.ad_text}
+              </p>
             </div>
           ))}
         </div>
