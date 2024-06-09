e4e89a05c3cd5498ccb7822105eaa2224bb4bbb9
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-06-09 22:11:59 +0300

    aa

--- src/pages/newcamp.jsx
+++ src/pages/newcamp.jsx
@@ -24,7 +24,7 @@ function parseCampaignData(rawData) {
   return rawData.map((item) => {
     try {
       const [textPart, imageUrlPart] = item.split("; ");
-      const text = textPart.split(": ")[2];
+      const text = textPart.split(": ")[1].replace(/^"(.*)"$/, '$1'); // Split and remove quotes
       const image_url = imageUrlPart.split(": ")[1];
       return { text, image_url };
     } catch (e) {
@@ -33,6 +33,7 @@ function parseCampaignData(rawData) {
     }
   });
 }
+
 
 function CreateCampaign() {
   const { user } = useAuth();
