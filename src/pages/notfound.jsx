94c5c56d04b01a60442800ec12ff2b12bf4bb287
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-06-18 14:00:21 +0300

    Navigation dev

--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -3,15 +3,20 @@ import '../index.css';
 import NavLogo from "../components/navLogo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Add this line
 import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+    const navigate = useNavigate();
+    const handleBack = navigate("/dashboard");
+
     return(
         <div className="w-full h-screen flex justify-center items-center">
             <div className="flex flex-col gap-5">
                 <NavLogo/>
                 <h2 className="font-custom font-bold text-center">Oops, we are still working on this page...</h2>
-                <button>
+                <button onClick={handleBack}>
                     <FontAwesomeIcon
+                      
                       icon={faArrowCircleLeft}
                       className="font-light text-3xl"
                     />
