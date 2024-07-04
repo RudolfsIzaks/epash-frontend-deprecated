63c4a18fef899512dc5d06a0ffc1eafc81668c7e
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-07-04 18:24:39 +0200

    sfdgfh

--- src/pages/success_google.jsx
+++ src/pages/success_google.jsx
@@ -10,7 +10,7 @@ function SuccessGoogle() {
             <div className="flex flex-col items-center gap-4">
                 <h1 className="font-custom font-black text-3xl text-center">Success! Google Connected.</h1>
                 <p className="font custom text-xl text-center">You are now able to run ads from our platform.</p>
-                <Link to="/success-google" className="flex justify-center items-center bg-epash-green text-white rounded-md px-8 h-12 font-bold hover:scale-110 duration-100">To Dashboard</Link>
+                <Link to="/dashboard" className="flex justify-center items-center bg-epash-green text-white rounded-md px-8 h-12 font-bold hover:scale-110 duration-100">To Dashboard</Link>
             </div>
           </div>
         </>
