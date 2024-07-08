e73bc3e439a758b6bc2929549c9cdf802eb9c67a
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-07-08 18:40:34 +0300

    dsfgfq

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -18,6 +18,8 @@ function UserProfile() {
     const scope = 'https://www.googleapis.com/auth/adwords';
     const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
     window.location.href = authUrl;
+    console.log(`Redirecting to: ${authUrl}`); // Log the URL
+    window.location.href = authUrl;
   };
 
   const handleLogout = async () => {
