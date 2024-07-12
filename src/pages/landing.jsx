54e4a6cee8da76266930d90424153d2c5a11d93f
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-07-13 02:00:10 +0300

    new mac addition

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -6,7 +6,6 @@ function LandingPage() {
     return(
         <>
          <NavbarLanding/>
-         <p>rhahahahhavrjd</p>
          <div className="bg-[url('https://res.cloudinary.com/drcze5fsl/image/upload/v1718707751/qztectbjngj6fbn85iwr.png')] bg-center bg-no-repeat bg-contain flex flex-col items-center h-screen justify-center">
             <p className="font-custom font-bold text-epash-green py-10 text-lg">Revolutionizing the AI game and...</p>
             <h1 className="font-custom text-5xl font-bold text-center px-48">Crushing Online Sales Records With Personalized AI Marketing</h1>
