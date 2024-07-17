2fbbb672bb2c809b478322f85331aae2d2747558
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-07-17 12:01:29 +0300

    Adding asset info into the platform edit

--- src/pages/platform_google_edit.jsx
+++ src/pages/platform_google_edit.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import NavLogo from "../components/navLogo";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import '../index.css';
 
 function PlatformGoogle() {
+  const location = useLocation();
+  const { data } = location.state || {};
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const {
+    Headings: headings,
+    LongHeadings: longHeadings,
+    Descriptions: descriptions,
+    Images: images,
+  } = JSON.parse(data);
+
   return (
     <>
       <div className="flex w-full justify-between items-center py-5 px-48">
@@ -20,39 +34,38 @@ function PlatformGoogle() {
       </div>
       <hr />
       <div>
-      <h1 className="text-center mt-20 text-5xl font-custom font-bold">Modify your google ads</h1>
-      <p className="text-center mb-20 mt-5 text-xl">Change headlines, body text and contents of your google ads.</p>
-      <div className=" flex flex-col m-10 rounded-lg shadow-lg border border-stone-200 bg-white p-10">
-        <h1 className="text-4xl font-custom">Google Ad 1:</h1>
-        <div className="flex gap-10 items-center justify-between mt-10">
+        <h1 className="text-center mt-20 text-5xl font-custom font-bold">Modify your google ads</h1>
+        <p className="text-center mb-20 mt-5 text-xl">Change headlines, body text and contents of your google ads.</p>
+        <div className=" flex flex-col m-10 rounded-lg shadow-lg border border-stone-200 bg-white p-10">
+          <h1 className="text-4xl font-custom">Google Ad 1:</h1>
+          <div className="flex gap-10 items-center justify-between mt-10">
             <div className="flex flex-col gap-4">
-                <h2 className="text-xl font-custom">Headlines</h2>
-                <input type="text" placeholder="Headline revision 1" className="rounded-md bg-stone-50 font-custom appearance-none outline-none border border-stone-200 py-2 px-5"/>
-                <input type="text" placeholder="Headline revision 2" className="rounded-md bg-stone-50 font-custom appearance-none outline-none border border-stone-200 py-2 px-5"/>
-                <input type="text" placeholder="Headline revision 3" className="rounded-md bg-stone-50 font-custom appearance-none outline-none border border-stone-200 py-2 px-5"/>
-                <h2 className="text-xl font-custom mt-5">Descriptions</h2>
-                <input type="text" placeholder="Description revision 1" className="rounded-md bg-stone-50 font-custom appearance-none outline-none border border-stone-200 py-2 px-5"/>
-                <input type="text" placeholder="Description revision 2" className="rounded-md bg-stone-50 font-custom appearance-none outline-none border border-stone-200 py-2 px-5"/>
-                <input type="text" placeholder="Description revision 3" className="rounded-md bg-stone-50 font-custom appearance-none outline-none border border-stone-200 py-2 px-5"/>
+              <h2 className="text-xl font-custom">Headlines</h2>
+              {headings.map((headline, index) => (
+                <input key={index} type="text" defaultValue={headline} className="rounded-md bg-stone-50 font-custom appearance-none outline-none border border-stone-200 py-2 px-5"/>
+              ))}
+              <h2 className="text-xl font-custom mt-5">Descriptions</h2>
+              {descriptions.map((description, index) => (
+                <input key={index} type="text" defaultValue={description} className="rounded-md bg-stone-50 font-custom appearance-none outline-none border border-stone-200 py-2 px-5"/>
+              ))}
             </div>
             <div className="flex flex-col items-end">
-                <div className="w-96 h-96 bg-green-200 rounded-md mb-5"></div>
-                <div className="flex gap-5">
-                    <button className="py-2 px-5 bg-epash-green rounded-md text-white font-custom font-black">
-                        Reroll
-                    </button>
-                    <button className="py-2 px-5 bg-epash-green rounded-md text-white font-custom font-black">
-                        Confirm
-                     </button>
-                </div>
+              <img src={images[0]} alt="Google Ad" className="w-96 h-96 object-cover rounded-md mb-5"/>
+              <div className="flex gap-5">
+                <button className="py-2 px-5 bg-epash-green rounded-md text-white font-custom font-black">
+                  Reroll
+                </button>
+                <button className="py-2 px-5 bg-epash-green rounded-md text-white font-custom font-black">
+                  Confirm
+                </button>
+              </div>
             </div>
-        </div>
-        <div>
+          </div>
+          <div>
             <button className="py-2 px-5 bg-epash-green rounded-md text-white font-custom font-black mt-10">Next</button>
+          </div>
         </div>
       </div>
-      </div>
-
     </>
   );
 }
