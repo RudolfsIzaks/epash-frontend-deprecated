4e5ceeeba3f3928c664d61d53ca628a0983d70d9
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-07-22 15:05:36 +0300

    aa

--- src/pages/platform_google_edit.jsx
+++ src/pages/platform_google_edit.jsx
@@ -54,13 +54,13 @@ function PlatformGoogle() {
               ))}
             </div>
             <div className="flex flex-col items-center mt-10 flex-grow">
-              <img src={images[0]} alt="Google Ad" className="w-96 h-96 object-cover rounded-md mb-5"/>
+              <img src={images[0]} alt="Google Ad" className="w-auto h-96 object-cover rounded-md mb-5"/>
               <div className="flex gap-5">
                 <button className="py-2 px-5 bg-epash-green rounded-md text-white font-custom font-black">
-                  Reroll
+                  Previous
                 </button>
                 <button className="py-2 px-5 bg-epash-green rounded-md text-white font-custom font-black">
-                  Confirm
+                  Next
                 </button>
               </div>
             </div>
