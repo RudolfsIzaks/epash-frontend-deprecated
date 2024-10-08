43c03c431ace1998f26c40f3befcb824ca4b09a7
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-10-08 17:15:19 +0300

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
 
     return(
         <>
-        <div className="w-full h-[20dvh] bg-epash-green flex items-center justify-evenly">
+        <div className="w-full h-[10dvh] bg-epash-green flex items-center justify-evenly">
             <Link className="text-white" to="/privacy-policy">Privacy Policy</Link>
             <Link className="text-white" to="/terms-conditions">Terms & Conditions</Link>
         </div>
