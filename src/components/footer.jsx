cb6ea860b94d25486e23bad579d40de1b3832132
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-10-08 17:16:52 +0300

    44

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,6 +7,7 @@ function Footer() {
     return(
         <>
         <div className="w-full h-[10dvh] bg-epash-green flex items-center justify-evenly">
+            <p className="font-custom text-white text-3xl">Epash AI</p>
             <Link className="text-white" to="/privacy-policy">Privacy Policy</Link>
             <Link className="text-white" to="/terms-conditions">Terms & Conditions</Link>
         </div>
